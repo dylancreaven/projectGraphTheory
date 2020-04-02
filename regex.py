08b0e22339f7a2a1fa1e463184bd03c81b95483a
dylancreaven <g00354442@gmit.ie>
2020-04-02 21:22:49 +0100

    Cleaned up regex.py by removing shunting code and importing shunting program thats also in repository

--- regex.py
+++ regex.py
@@ -1,6 +1,7 @@
 #Dylan Creaven
 #Classes in thompsons construction
 #need to re-do matching
+import shunting
 class State:
     """A State with one or two edges/arrows, with all edges/arrows labelled w/ label"""
   # Constructor.
@@ -17,48 +18,11 @@ class Fragment:
         self.start=start
         self.accept=accept
 
-def shunt(infix):
-    """Return infix regex as postfix"""
-    #convert input to a stack list
-    infix=list(infix)[::-1]
-    #operator stack and output list as empty lists
-    opers,postfix =[],[]
-    #operator precedence
-    prec={'*':100,'.':90,  '|':80,   ')':70,    '(':60}
-
-    #loop through input one character at a time
-    while infix:
-        #pop a character from the input
-        c=infix.pop() 
-        #decide what to do based on character
-        if c== '(':
-            #push an open bracket to opers stack
-            opers.append(c)
-        elif c==')':
-            #pop the operators stack until you find an open bracket
-            while opers[-1]!='(':
-                 postfix.append(opers.pop())
-            #get rid of '('
-            opers.pop()
-        elif c in prec:
-            #push any operators on opers stack with hight prec to output
-            while opers and prec[c] < prec[opers[-1]]:
-                postfix.append(opers.pop())
-            opers.append(c)
-        else:
-        #typically we just push the character to the output
-            postfix.append(c)
-    #pop all operators to the output
-    while opers:
-        postfix.append(opers.pop())
-    #convert output list to string
-    return ''.join(postfix)
-
 
 def compile(infix):
     """Return Fragment of NFA that represents infix regex"""
     #convert infix to postfix
-    postfix = shunt(infix)
+    postfix = shunting.shunt(infix)
     #make a listfix stack of chars and invert it
     postfix=list(postfix)[::-1] 
     #stack for nfa fragments
--- shunting.py
+++ shunting.py
@@ -1,57 +1,39 @@
 #Dylan Creaven
 # =The Shunting Yard algoritm for regex
 
-infix ="(a|b).c*"
-print("Input: "+infix)
+def shunt(infix):
+    """Return infix regex as postfix"""
+    #convert input to a stack list
+    infix=list(infix)[::-1]
+    #operator stack and output list as empty lists
+    opers,postfix =[],[]
+    #operator precedence
+    prec={'*':100,'.':90,  '|':80,   ')':70,    '(':60}
 
-#Expected output ab|c*.
-print("Expected Output: ab|c*.")
-
-#convert input to a stack-ish list
-infix=list(infix)[::-1]
-
-
-
-#operator stack
-opers =[]
-
-#Output List. 
-postfix=[]
-#operator precedence
-prec={'*':100,  '.':90,  '|':80,   ')':70,    '(':60}
-
-#loop through input one character at a time
-while infix:
-    #pop a character from the input
-    c=infix.pop() 
-    #decide what to do based on character
-    if c== '(':
-        #push an open bracket to opers stack
-        opers.append(c)
-    elif c==')':
-        #pop the operators stack until you find an open bracket
-        while opers[-1]!='(':
-              postfix.append(opers.pop())
-        #get rid of '('
-        opers.pop()
-    elif c in prec:
-       #push any operators on opers stack with hight prec to output
-         while opers and prec[c] < prec[opers[-1]]:
-             postfix.append(opers.push())
-         opers.append(c)
-    else:
-    #typically we just push the character to the output
-         postfix.append(c)
-
-#pop all operators to the output
-while (opers):
-    postfix.append(opers.pop())
-    
-
-
-#convert output list to string
-postfix=''.join(postfix)
-
-
-##Print result
-print("Output: "+postfix)
+    #loop through input one character at a time
+    while infix:
+        #pop a character from the input
+        c=infix.pop() 
+        #decide what to do based on character
+        if c== '(':
+            #push an open bracket to opers stack
+            opers.append(c)
+        elif c==')':
+            #pop the operators stack until you find an open bracket
+            while opers[-1]!='(':
+                 postfix.append(opers.pop())
+            #get rid of '('
+            opers.pop()
+        elif c in prec:
+            #push any operators on opers stack with hight prec to output
+            while opers and prec[c] < prec[opers[-1]]:
+                postfix.append(opers.pop())
+            opers.append(c)
+        else:
+        #typically we just push the character to the output
+            postfix.append(c)
+    #pop all operators to the output
+    while opers:
+        postfix.append(opers.pop())
+    #convert output list to string
+    return ''.join(postfix)
